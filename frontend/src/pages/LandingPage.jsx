f3867710eee36f58d4a7eb7ab4daa62b83a4f567
Oscar Aora <aoraoscar06@gmail.com>
2026-06-14 14:27:30 -0700

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import { ArrowRight, Star, Camera, Instagram, Twitter, Facebook } from "lucide-react";
 import Navbar from "../components/layouts/Navbar";
